46672bb43dfc9297b68d4c6b89ba4bc0de6bd57b
rt <you@example.com>
2025-12-03 17:53:18 +0900

    remove espnet from __init__.py

--- pysctkja/__init__.py
+++ pysctkja/__init__.py
@@ -9,5 +9,5 @@ from .app import app_pyscliteja as app_pyscliteja
 from .app import app_preprocess as app_preprocess
 from .app import app_postprocess as app_postprocess
 
-from .espnet_batch import espnet_main as espnet_batch_main
-from .espnet_stream import espnet_stream_main as espnet_stream_main
+#from .espnet_batch import espnet_main as espnet_batch_main
+#from .espnet_stream import espnet_stream_main as espnet_stream_main
